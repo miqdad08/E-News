0191f3a96dfb6a0136c22bb1a937f85c0ace1bba
salehrashid <saleh.rashid.b@gmail.com>
2023-08-16 21:35:18 +0700

    refactor(api-service): change country ID to US

--- app/src/main/java/com/miqdad/e_news/ui/home/HomeFragment.kt
+++ app/src/main/java/com/miqdad/e_news/ui/home/HomeFragment.kt
@@ -8,15 +8,14 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.miqdad.e_news.R
@@ -133,6 +132,6 @@ class HomeFragment : Fragment() {
     }
 
     private fun finish() {
-        TODO("Not yet implemented")
+        System.exit(-1)
     }
 }
